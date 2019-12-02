9302778a989e001098c4db5801bfc54af5caa8b0
Elias Meire <me@eliasmei.re>
2019-12-02 10:15:34 +0100

    Defer font loading for better performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -162,9 +162,21 @@ export default class GivtoApp extends App<
             />
             <link rel="manifest" href="/givto.webmanifest" />
             <link
+              rel="preload"
               href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=block"
-              rel="stylesheet"
+              as="style"
+              onLoad={event => {
+                const link = event.target as HTMLLinkElement;
+                link.onload = null;
+                link.rel = 'stylesheet';
+              }}
             />
+            <noscript>
+              <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=block"
+              />
+            </noscript>
             <meta name="msapplication-TileColor" content="#603cba" />
             <meta name="theme-color" content="#5A51FF"></meta>
           </Head>
