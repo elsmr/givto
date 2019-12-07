183078022de599f7696c1da65a9346ac8035a396
Elias Meire <me@eliasmei.re>
2019-12-07 12:50:40 +0100

    Improve font loading

--- lib/frontend/theme.ts
+++ lib/frontend/theme.ts
@@ -3,7 +3,7 @@ export const theme = {
   fonts: {
     body:
       'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-    heading: 'Lato, system-ui',
+    heading: 'Lato, system-ui, sans-serif',
     monospace: 'Menlo, monospace'
   },
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
